80137df37fa93fa93f36f6be6282c86be5e0787f
Jacen Barefoot <89105120+JacenBarefoot@users.noreply.github.com>
2022-02-10 16:19:29 -0600

    Getting there

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE CUSTOMERS, ORDERS, BOARDS, SHOES;
+DROP TABLE CUSTOMERS, ORDERS, BOARDS, SHOES, BACKPACKS, SHIRTS, PANTS, HOODIES, SOCKS, HATS, HELMETS;
 
 CREATE TABLE CUSTOMERS(
     id BIGSERIAL PRIMARY KEY,
@@ -11,45 +11,153 @@ CREATE TABLE CUSTOMERS(
 CREATE TABLE ORDERS(
     id BIGSERIAL PRIMARY KEY
 );
-
+-------------------------------------------------------------------------
 CREATE TABLE BOARDS(
     id BIGSERIAL PRIMARY KEY,
     boardBrand TEXT NOT NULL,
     boardSize float NOT NULL,
     wheelSize int NOT NULL,
-    wheelColor TEXT NOT NULL
+    wheelColor TEXT NOT NULL,
+    price FLOAT NOT NULL
 );
 
+INSERT INTO BOARDS (boardBrand, boardSize, wheelSize, wheelColor, price)
+VALUES ('Baker', 8.5, 56, 'Green', 79.95),
+       ('Santa Cruz', 8.38, 55, 'Blue', 84.95),
+       ('Element', 8.125, 54, 'Red', 89.95),
+       ('Birdhouse', 8.0, 56, 'Brown', 81.95),
+       ('Zero', 8.5, 55, 'Black', 74.95);
+
+-------------------------------------------------------------------------
 CREATE TABLE SHOES(
     id BIGSERIAL PRIMARY KEY,
     shoeBrand TEXT NOT NULL,
     shoeType TEXT NOT NULL,
     shoeSize float NOT NULL,
-    shoeColor TEXT NOT NULL
+    shoeColor TEXT NOT NULL,
+    price FLOAT NOT NULL
 );
 
+INSERT INTO SHOES (shoeBrand, shoeType, shoeSize, shoeColor, price)
+VALUES ('Vans', 'Athletic', 11.5, 'Red', 64.95),
+       ('Nike', 'Athletic', 7.5, 'Blue', 74.95),
+       ('Cariuma', 'Athletic', 8.5, 'Grey', 69.95),
+       ('DC', 'Athletic', 10, 'Orange', 54.95),
+       ('Emerica', 'Athletic', 12, 'Brown', 59.95);
 
-INSERT INTO BOARDS (boardBrand, boardSize, wheelSize, wheelColor)
-VALUES ('Alien Workshop', 8.5, 56, 'Green'),
-       ('Santa Cruz', 8.38, 55, 'Blue'),
-       ('Almost', 8.125, 54, 'Red'),
-       ('Antihero', 8.0, 56, 'Brown'),
-       ('Baker', 8.5, 55, 'Black'),
-       ('Element', 8.38, 54, 'Green'),
-       ('Toy Machine', 8.125, 56, 'Blue'),
-       ('Zero', 8.0, 55, 'Red'),
-       ('Flip', 8.5, 54, 'Brown'),
-       ('Birdhouse', 8.38, 53, 'Black');
+-------------------------------------------------------------------------
+CREATE TABLE BACKPACKS(
+    id BIGSERIAL PRIMARY KEY,
+    bagBrand TEXT NOT NULL,
+    bagSize TEXT NOT NULL,
+    bagColor TEXT NOT NULL,
+    isWaterproof TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
 
-INSERT INTO SHOES (shoeBrand, shoeType, shoeSize, shoeColor)
-VALUES ('Vans', 'Athletic', 11.5, 'Red'),
-       ('Nike', 'Athletic', 7.5, 'Blue'),
-       ('Cariuma', 'Athletic', 8.5, 'Grey'),
-       ('DC', 'Athletic', 10, 'Orange'),
-       ('Converse', 'Athletic', 12, 'Brown');
+INSERT INTO BACKPACKS (bagBrand, bagSize, bagColor, isWaterproof, price)
+VALUES ('Vans', 'Large', 'Brown', 'Yes', 64.95),
+       ('Nike', 'Medium', 'Red', 'No', 54.95),
+       ('Under Armour', 'Large', 'Blue', 'Yes', 44.95),
+       ('Hurley', 'Small', 'Green', 'No', 26.95),
+       ('Element', 'Large', 'Orange', 'Yes', 18.95);
 
-INSERT INTO BACKPACKS (bagBrand, bagSize, bagColor)
-VALUES ('Element', 'Large', 'Brown')
+-------------------------------------------------------------------------
+CREATE TABLE SHIRTS(
+    id BIGSERIAL PRIMARY KEY,
+    shirtBrand TEXT NOT NULL,
+    shirtType TEXT NOT NULL,
+    shirtSize TEXT NOT NULL,
+    shirtColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+INSERT INTO SHIRTS (shirtBrand, shirtType, shirtSize, shirtColor, price)
+VALUES ('Nike SB', 'Short Sleeve', 'Medium', 'Blue', 19.95),
+       ('Polar Skate', 'Long Sleeve', 'Large', 'Green', 14.95),
+       ('Santa Cruz', 'Flannel', 'Medium', 'Red', 17.95),
+       ('Thrasher', 'Long Sleeve', 'Small', 'Purple', 21.95),
+       ('Powell Peralta', 'Short Sleeve', 'XL', 'Brown', 16.95);
+
+-------------------------------------------------------------------------
+CREATE TABLE PANTS(
+    id BIGSERIAL PRIMARY KEY,
+    pantsBrand TEXT NOT NULL,
+    pantsType TEXT NOT NULL,
+    pantsSize TEXT NOT NULL,
+    pantsColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO PANTS (pantsBrand, pantsType, pantsSize, pantsColor, price)
+VALUES ('Nike SB', 'Flexible', '28-30', 'Blue', 39.95),
+       ('Volcom', 'Cargo', '32-32', 'Blue', 34.95),
+       ('Dickies', 'Slim Straight', '36-32', 'Blue', 37.95),
+       ('Vans', 'Lounge', '32-34', 'Blue', 31.95),
+       ('Adidas', 'Khakis', '30-36', 'Blue', 36.95);
+
+-------------------------------------------------------------------------
+CREATE TABLE HOODIES(
+    id BIGSERIAL PRIMARY KEY,
+    hoodieBrand TEXT NOT NULL,
+    hoodieSize TEXT NOT NULL,
+    hoodieColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO HOODIES (hoodieBrand, hoodieSize, hoodieColor, price)
+VALUES ('Nike SB', 'Large', 'Blue', 99.95),
+       ('Volcom', 'Small', 'Red', 94.95),
+       ('Dickies', 'Medium', 'Orange', 97.95),
+       ('Vans', 'XL', 'Yellow', 91.95),
+       ('Polar Skate', 'Medium', 'Green', 106.95);
+
+-------------------------------------------------------------------------
+CREATE TABLE SOCKS(
+    id BIGSERIAL PRIMARY KEY,
+    socksBrand TEXT NOT NULL,
+    socksSize TEXT NOT NULL,
+    socksColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO SOCKS (socksBrand, socksSize, socksColor, price)
+VALUES ('Pizza', 'Short', 'Blue', 12.95),
+       ('Vans', 'Long', 'Red', 11.95),
+       ('Lakai', 'Medium', 'Orange', 9.95),
+       ('Nike', 'Short', 'Yellow', 14.95),
+       ('Polar Skate', 'Long', 'Green', 10.95);
+
+-------------------------------------------------------------------------
+CREATE TABLE HATS(
+    id BIGSERIAL PRIMARY KEY,
+    hatsBrand TEXT NOT NULL,
+    hatsType TEXT NOT NULL,
+    hatsColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO HATS (hatsBrand, hatsType, hatsColor, price)
+VALUES ('Pizza', 'Snapback', 'Blue', 12.95),
+       ('Vans', 'Strapback', 'Green', 11.95),
+       ('Lakai', 'Curved Bill', 'Orange', 9.95),
+       ('Thrasher', 'Snapback', 'Yellow', 14.95),
+       ('Nike', 'Beanie', 'Red', 10.95);
+
+-------------------------------------------------------------------------
+CREATE TABLE HELMETS(
+    id BIGSERIAL PRIMARY KEY,
+    helmetBrand TEXT NOT NULL,
+    helmetSize TEXT NOT NULL,
+    helmetColor TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO HELMETS (helmetBrand, helmetSize, helmetColor, price)
+VALUES ('Triple 8', 'Large', 'Orange', 12.95),
+       ('Triple 8', 'Small', 'Blue', 11.95),
+       ('The one Jacen needed', 'Medium', 'Red/Black', 9.95);
+
+-------------------------------------------------------------------------
 
 
-<--!--> Make sure to create the Backpacks table <--!-->
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,18 @@ import java.util.*;
 import java.sql.*;
 
 public class Main {
+    public static List<String> order = new ArrayList<>();
+
     public static void main(String[] args) {
+        List<String> order = new ArrayList<>();
+
         String action = "let's get this working";
         Scanner string = new Scanner(System.in);
         System.out.println("Hello there!");
         System.out.println("Welcome to JacensSkateboards!");
         System.out.println("What would you like to do?");
         while (!action.equals("quit")) {
-            System.out.println("You can [create account], [browse] shop, [create skateboard], [order], or [quit]");
+            System.out.println("You can [create account], [browse] shop, [create skateboard], [cart], or [quit]");
             action = string.nextLine();
             switch (action) {
                 case "create account":
@@ -21,6 +25,9 @@ public class Main {
                     break;
                 case "create skateboard":
                     createSkateboard();
+                    break;
+                case "cart":
+                    order();
                     break;
                 case "quit":
                     break;
@@ -76,40 +83,254 @@ public class Main {
             e.printStackTrace();
         }
     }
+    public static void order() {
+        Iterator<String> variable = order.iterator();
+        float total = 0;
+        while (variable.hasNext()){
+            Object e = variable.next();
+            System.out.println(e);
+            String a = e.toString();
+            System.out.println("---------------------------------");
+            total += Float.parseFloat(a.substring(a.indexOf("$") + 1, a.indexOf("$") + 6).trim());
+
+        }
+//        System.out.printf("%.2f", total);
+//        System.out.println();
+        System.out.print("Total: $" + total);
+        System.out.println();
+    }
     public static void browse() {
         Scanner string = new Scanner(System.in);
-        System.out.println("What would you like to browse? [Boards], [Shoes], or [Backpacks]");
+        System.out.println("What would you like to browse? [Boards], [Shoes], [Clothing], or [Backpacks]");
         String action = string.nextLine();
         switch(action) {
+            case "Clothing":
+                System.out.println("Choose from one of these: [Shirts] [Pants] [Hoodies] [Socks]");
+                String action1 = string.nextLine();
+                switch(action1){
+                    case "Shirts":
+                        Scanner number = new Scanner(System.in);
+                        try {
+                            Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                            Statement st = connection.createStatement();
+                            ResultSet rs = st.executeQuery("SELECT * FROM SHIRTS");
+                            while (rs.next())
+                            {
+                                System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("shirtBrand") + " |");
+                                System.out.print(" Shirt Type: " + rs.getString("shirtType") + " |");
+                                System.out.print(" Shirt Size: " + rs.getString("shirtSize") + " |");
+                                System.out.println(" Shirt Color: " + rs.getString("shirtColor"));
+                                System.out.println("------------------------------------------------------------------------------------");
+                            }
+                            rs.close();
+                            st.close();
+                        } catch(SQLException e){
+                            e.printStackTrace();
+                        }
+                        System.out.println("What would like to order? or type [100] to quit");
+                        System.out.print("> ");
+                        int itemNum1 = number.nextInt();
+                        if (itemNum1 == 100) {
+                            break;
+                        } else if (itemNum1 >= 1 && itemNum1 <= 60) {
+                            try {
+                                Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                                PreparedStatement st = connection.prepareStatement("SELECT * FROM SHIRTS WHERE id = ?");
+                                st.setInt(1, itemNum1);
+                                ResultSet rs = st.executeQuery();
+                                while (rs.next()) {
+                                    double price = Double.parseDouble(rs.getString("price"));
+                                    order.add(rs.getString("shirtBrand") + " Shirt" + " | $" + price + " | ");
+                                }
+                                rs.close();
+                                st.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println("---Added to Cart Successfully---");
+                        }
+                        break;
+                    case "Pants":
+                        Scanner number2 = new Scanner(System.in);
+                        try {
+                            Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                            Statement st = connection.createStatement();
+                            ResultSet rs = st.executeQuery("SELECT * FROM PANTS");
+                            while (rs.next())
+                            {
+                                System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("pantsBrand") + " |");
+                                System.out.print(" Pants Type: " + rs.getString("pantsType") + " |");
+                                System.out.print(" Pants Size: " + rs.getString("pantsSize") + " |");
+                                System.out.println(" Pants Color: " + rs.getString("pantsColor"));
+                                System.out.println("------------------------------------------------------------------------------------");
+                            }
+                            rs.close();
+                            st.close();
+                        } catch(SQLException e){
+                            e.printStackTrace();
+                        }
+                        System.out.println("What would like to order? or type [100] to quit");
+                        System.out.print("> ");
+                        int itemNum2 = number2.nextInt();
+                        if (itemNum2 == 100) {
+                            break;
+                        } else if (itemNum2 >= 1 && itemNum2 <= 60) {
+                            try {
+                                Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                                PreparedStatement st = connection.prepareStatement("SELECT * FROM PANTS WHERE id = ?");
+                                st.setInt(1, itemNum2);
+                                ResultSet rs = st.executeQuery();
+                                while (rs.next()) {
+                                    double price = Double.parseDouble(rs.getString("price"));
+                                    order.add(rs.getString("pantsBrand") + " Pants" + " | $" + price + " | ");
+                                }
+                                rs.close();
+                                st.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println("---Added to Cart Successfully---");
+                            break;
+                        }
+//                    case "Hoodies":
+//                        Scanner number3 = new Scanner(System.in);
+//                        try {
+//                            Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+//                            Statement st = connection.createStatement();
+//                            ResultSet rs = st.executeQuery("SELECT * FROM HOODIES");
+//                            while (rs.next())
+//                            {
+//                                System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("hoodiesBrand") + " |");
+//                                System.out.print(" Pants Type: " + rs.getString("pantsType") + " |");
+//                                System.out.print(" Pants Size: " + rs.getString("pantsSize") + " |");
+//                                System.out.println(" Pants Color: " + rs.getString("pantsColor"));
+//                                System.out.println("------------------------------------------------------------------------------------");
+//                            }
+//                            rs.close();
+//                            st.close();
+//                        } catch(SQLException e){
+//                            e.printStackTrace();
+//                        }
+//                        System.out.println("What would like to order? or type [100] to quit");
+//                        System.out.print("> ");
+//                        int itemNum2 = number2.nextInt();
+//                        if (itemNum2 == 100) {
+//                            break;
+//                        } else if (itemNum2 >= 1 && itemNum2 <= 60) {
+//                            try {
+//                                Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+//                                PreparedStatement st = connection.prepareStatement("SELECT * FROM PANTS WHERE id = ?");
+//                                st.setInt(1, itemNum2);
+//                                ResultSet rs = st.executeQuery();
+//                                while (rs.next()) {
+//                                    double price = Double.parseDouble(rs.getString("price"));
+//                                    order.add(rs.getString("pantsBrand") + " Pants" + " | $" + price + " | ");
+//                                }
+//                                rs.close();
+//                                st.close();
+//                            } catch (SQLException e) {
+//                                e.printStackTrace();
+//                            }
+//                            System.out.println("---Added to Cart Successfully---");
+//                            break;
+                        }
+                }
+                break;
             case "Boards":
+                Scanner num = new Scanner(System.in);
                 try {
                     Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
                     Statement st = connection.createStatement();
                     ResultSet rs = st.executeQuery("SELECT * FROM BOARDS");
-                    while (rs.next())
-                    {
-                        System.out.print(rs.getInt("id") + "." + "Brand name: " + rs.getString("boardBrand") + " |");
-                        System.out.print(" Board Size: " + rs.getString("boardSize") + " |");
-                        System.out.print(" Wheel Size: " + rs.getString("wheelSize") + " |");
+                    while (rs.next()) {
+                        System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("boardBrand") + " |");
+                        System.out.print(" Board Size: " + rs.getString("boardSize") + "in |");
+                        System.out.print(" Wheel Size: " + rs.getString("wheelSize") + "cm |");
                         System.out.println(" Wheel Color: " + rs.getString("wheelColor"));
                         System.out.println("------------------------------------------------------------------------------------");
                     }
                     rs.close();
                     st.close();
-                } catch(SQLException e){
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("What would like to order? or type [100] to quit");
+                System.out.print("> ");
+                int itemNumb = num.nextInt();
+                if (itemNumb == 100) {
+                    break;
+                } else if (itemNumb >= 1 && itemNumb <= 60) {
+                    try {
+                        Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                        PreparedStatement st = connection.prepareStatement("SELECT * FROM BOARDS WHERE id = ?");
+                        st.setInt(1, itemNumb);
+                        ResultSet rs = st.executeQuery();
+                        while (rs.next()) {
+                            double price = Double.parseDouble(rs.getString("price"));
+                            order.add(rs.getString("boardBrand") + " Board " + " | $" + price + " | ");
+                        }
+                        rs.close();
+                        st.close();
+                    } catch (SQLException e) {
                         e.printStackTrace();
+                    }
+                    System.out.println("---Added to Cart Successfully---");
+                    break;
                 }
             case "Shoes":
+                Scanner i = new Scanner(System.in);
                 try {
                     Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
                     Statement st = connection.createStatement();
                     ResultSet rs = st.executeQuery("SELECT * FROM SHOES");
+                    while (rs.next()) {
+                        System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("shoeBrand") + " |");
+                        System.out.print(" Shoe Type: " + rs.getString("shoeType") + " |");
+                        System.out.print(" Shoe Size: " + rs.getString("shoeSize") + "in |");
+                        System.out.println(" Shoe Color: " + rs.getString("shoeColor"));
+                        System.out.println("------------------------------------------------------------------------------------");
+                    }
+                    rs.close();
+                    st.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("What would like to order? or type [100] to quit");
+                System.out.print("> ");
+                int itemNumber = i.nextInt();
+                if (itemNumber == 100) {
+                    break;
+                } else if (itemNumber >= 1 && itemNumber <= 60) {
+                    try {
+                        Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                        PreparedStatement st = connection.prepareStatement("SELECT * FROM SHOES WHERE id = ?");
+                        st.setInt(1, itemNumber);
+                        ResultSet rs = st.executeQuery();
+                        while (rs.next()) {
+                            double price = Double.parseDouble(rs.getString("price"));
+                            order.add(rs.getString("shoeBrand") + " Shoes" + " | $" + price + " | ");
+                        }
+                        rs.close();
+                        st.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("---Added to Cart Successfully---");
+                    break;
+                }
+            case "Backpacks":
+                Scanner number = new Scanner(System.in);
+
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                    Statement st = connection.createStatement();
+                    ResultSet rs = st.executeQuery("SELECT * FROM BACKPACKS");
                     while (rs.next())
                     {
-                        System.out.print(rs.getInt("id") + "." + "Brand name: " + rs.getString("shoeBrand") + " |");
-                        System.out.print(" Shoe Type: " + rs.getString("shoeType") + " |");
-                        System.out.print(" Shoe Size: " + rs.getString("shoeSize") + " |");
-                        System.out.println(" Shoe Color: " + rs.getString("shoeColor"));
+                        System.out.print(rs.getInt("id") + "." + " Brand name: " + rs.getString("bagBrand") + " |");
+                        System.out.print(" Backpack Size: " + rs.getString("bagSize") + " |");
+                        System.out.print(" Backpack Color: " + rs.getString("bagColor") + " |");
+                        System.out.println(" Waterproof: " + rs.getString("isWaterproof"));
                         System.out.println("------------------------------------------------------------------------------------");
                     }
                     rs.close();
@@ -117,10 +338,32 @@ public class Main {
                 } catch(SQLException e){
                     e.printStackTrace();
                 }
-
+                System.out.println("What would like to order? or type [100] to quit");
+                System.out.print("> ");
+                int itemNum = number.nextInt();
+                if (itemNum == 100) {
+                    break;
+                } else if (itemNum >= 1 && itemNum <= 60) {
+                    try {
+                        Connection connection = DriverManager.getConnection("jdbc:postgresql:Skateboards");
+                        PreparedStatement st = connection.prepareStatement("SELECT * FROM BACKPACKS WHERE id = ?");
+                        st.setInt(1, itemNum);
+                        ResultSet rs = st.executeQuery();
+                        while (rs.next()) {
+                            double price = Double.parseDouble(rs.getString("price"));
+                            order.add(rs.getString("bagBrand") + " Backpack" + " | $" + price + " | ");
+                        }
+                        rs.close();
+                        st.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("---Added to Cart Successfully---");
+                    break;
+                }
         }
     }
 }
-// Make sure to create the backpack table in schema
+
 // Make sure to make the order system, adding lists to an Order ArrayList
 // Maybe custom shoes
